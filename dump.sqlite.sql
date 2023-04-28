b5a989a8f24d3987136b49164a15937f70996607
Simon Chidi <55095121+iamSimonChidi@users.noreply.github.com>
2023-04-28 12:14:51 +0100

    Update dump.sqlite.sql

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -34,7 +34,8 @@ QUESTION SEVEN: Query a list of CITY and STATE from the STATION table.
 SELECT CITY, STATE
 FROM STATION;
 
-QUESTION EIGHT: Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+QUESTION EIGHT: Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, 
+but exclude duplicates from the answer.
 SELECT DISTINCT CITY
 FROM STATION
 WHERE MOD(ID, 2) = 0;
@@ -43,7 +44,8 @@ QUESTION NINE: Find the difference between the total number of CITY entries in t
 SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
 FROM STATION
 
-QUESTION TEN: Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+QUESTION TEN: Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 ( select CITY, char_length(CITY) as len_city 
   from STATION 
   where char_length(CITY) = (select char_length(CITY) 
@@ -69,10 +71,12 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
 
-QUESTION 13: Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+QUESTION 13: Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. 
+Your result cannot contain duplicates.
 SELECT DISTINCT CITY
 FROM STATION
-WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u')
+WHERE (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%') AND (CITY LIKE '%a' OR CITY LIKE '%e' 
+                                                                                                    OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u')
 ORDER BY CITY;
 
 QUESTION 14: Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
@@ -97,7 +101,8 @@ FROM STATION
 WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u')
 AND LOWER(SUBSTR(CITY,LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');
 
-QUESTION 18: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+QUESTION 18: Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 SELECT Name
 FROM STUDENTS
 WHERE Marks > 75
@@ -108,7 +113,8 @@ SELECT name
 FROM Employee
 ORDER BY name ASC
 
-QUESTION 20: Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id
+QUESTION 20: Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater 
+than  per month who have been employees for less than  months. Sort your result by ascending employee_id
 SELECT name
 FROM Employee
 WHERE salary > 2000 AND months < 10
